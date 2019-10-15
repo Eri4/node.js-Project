82625150574ef4c0507febc22d9f3d2f5c8df577
Albi Luzi <albi.luzi@innovaway.al>
2019-10-15 14:53:23 +0200

    comit2

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiZXJpMDQiLCJhIjoiY2swbTg4aTc1MTEyMTNjcG50cDR2Y3EzbyJ9.xEFiyBNRCG6WAokAzcs_Ew&limit=1'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
