da61b1a65a4b9e2c0bbda595e595658cb0cc5c71
Ermir <ermir.jace0@gmail.com>
2019-10-18 17:29:35 +0200

    comit4

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const url = 'https://api.darksky.net/forecast/69ba80b0ee78fddea06073348d38197b/' + latitude + ',' + longitude
+    const url = `https://api.darksky.net/forecast/69ba80b0ee78fddea06073348d38197b/${latitude},${longitude}?units=si`
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
@@ -9,7 +9,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            // console.log(body.daily.data)
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degress out. There is a ${body.currently.precipProbability} chance of rain. The lowest temperature for today is ${body.daily.data[0].apparentTemperatureLow} degress and the highes is ${body.daily.data[0].apparentTemperatureHigh} degress.`)
         }
     })
 }
